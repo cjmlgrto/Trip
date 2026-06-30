2bdcd36f623e0b8ec233dd3d026d69832653dc1e
Carlos Melegrito <cjmlgrto@apple.com>
2026-06-30 08:29:35 +0100

    Reduce itinerary scroll top margin to 16pt

--- Trip/View/TripListView.swift
+++ Trip/View/TripListView.swift
@@ -78,7 +78,7 @@ struct TripListView: View {
             .listRowInsets(.init(top: 16, leading: 16, bottom: 24, trailing: 16))
         }
         .listStyle(.plain)
-        .contentMargins(.top, 24, for: .scrollContent)
+        .contentMargins(.top, 16, for: .scrollContent)
         .simultaneousGesture(
             MagnifyGesture()
                 .onChanged { value in
